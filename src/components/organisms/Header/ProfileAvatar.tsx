46e68e31daba3de7c662bda4d5071016031a884a
VoHoangKiet <muoivo.051064@gmail.com>
2025-05-18 11:18:43 +0700

    refactor: [AICP-15] use isAuth to check logged and improve dropdown menu logic

--- src/components/organisms/Header/ProfileAvatar.tsx
+++ src/components/organisms/Header/ProfileAvatar.tsx
@@ -1,57 +1,59 @@
 import { LoginOutlined, LogoutOutlined, UserAddOutlined, UserOutlined } from '@ant-design/icons';
 import { Avatar, Dropdown } from 'antd';
 import { useTranslation } from 'react-i18next';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
+import { RootState } from '@app/redux/store';
 import type { MenuProps } from 'antd';
 
 import './ProfileAvatar.scss';
 
 const ProfileAvatar = () => {
   const { t } = useTranslation();
+  const { isAuth } = useSelector((state: RootState) => state.auth);
 
-  const items: MenuProps['items'] = [
-    {
-      key: '1',
-      label: (
-        <div className='flex items-center gap-3 text-lg font-semibold !p-1 hover:!rounded-lg'>
-          <UserOutlined />
-          <div>{t('PROFILE.PERSONAL_PROFILE')}</div>
-        </div>
-      ),
-      style: { display: !localStorage.getItem('access_token') ? 'none' : 'block' },
-    },
-    {
-      key: '2',
-      label: (
-        <div className='flex items-center gap-3 text-lg font-semibold !p-1 text-[#ED0909]'>
-          <LogoutOutlined />
-          <div>{t('PROFILE.LOGOUT')}</div>
-        </div>
-      ),
-      style: { display: !localStorage.getItem('access_token') ? 'none' : 'block' },
-    },
-    {
-      key: '3',
-      label: (
-        <Link className='flex items-center gap-3 text-lg font-semibold !p-1' to={'/login'}>
-          <LoginOutlined />
-          <div>{t('DROPDOWN_PROFILE.SIGN_IN')}</div>
-        </Link>
-      ),
-      style: { display: localStorage.getItem('access_token') ? 'none' : 'block' },
-    },
-    {
-      key: '4',
-      label: (
-        <Link className='flex items-center gap-3 text-lg font-semibold !p-1' to={'/register'}>
-          <UserAddOutlined />
-          <div>{t('DROPDOWN_PROFILE.SIGN_UP')}</div>
-        </Link>
-      ),
-      style: { display: localStorage.getItem('access_token') ? 'none' : 'block' },
-    },
-  ];
+  const items: MenuProps['items'] = isAuth
+    ? [
+        {
+          key: '1',
+          label: (
+            <div className='flex items-center gap-3 text-lg font-semibold !p-1 hover:!rounded-lg'>
+              <UserOutlined />
+              <div>{t('PROFILE.PERSONAL_PROFILE')}</div>
+            </div>
+          ),
+        },
+        {
+          key: '2',
+          label: (
+            <div className='flex items-center gap-3 text-lg font-semibold !p-1 text-[#ED0909]'>
+              <LogoutOutlined />
+              <div>{t('PROFILE.LOGOUT')}</div>
+            </div>
+          ),
+        },
+      ]
+    : [
+        {
+          key: '3',
+          label: (
+            <Link className='flex items-center gap-3 text-lg font-semibold !p-1' to={'/login'}>
+              <LoginOutlined />
+              <div>{t('DROPDOWN_PROFILE.SIGN_IN')}</div>
+            </Link>
+          ),
+        },
+        {
+          key: '4',
+          label: (
+            <Link className='flex items-center gap-3 text-lg font-semibold !p-1' to={'/register'}>
+              <UserAddOutlined />
+              <div>{t('DROPDOWN_PROFILE.SIGN_UP')}</div>
+            </Link>
+          ),
+        },
+      ];
 
   return (
     <Dropdown menu={{ items }} placement='bottomRight' arrow>
