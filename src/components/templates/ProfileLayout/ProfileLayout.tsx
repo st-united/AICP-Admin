4dec0ba7e4d17ecf8f300119fb25f77237916482
VoHoangKiet <muoivo.051064@gmail.com>
2025-05-19 08:45:53 +0700

    fix: [AICP-15] fix layout form

--- src/components/templates/ProfileLayout/ProfileLayout.tsx
+++ src/components/templates/ProfileLayout/ProfileLayout.tsx
@@ -9,7 +9,7 @@ const ProfileLayout = () => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
 
   return (
-    <div className='relative flex justify-center items-center w-full min-h-[80vh] bg-[#f3f8fa]'>
+    <div className='flex justify-center items-center w-full min-h-[80vh] bg-[#f3f8fa]'>
       <div
         className={`
           fixed inset-0 bg-black bg-opacity-50 z-40
@@ -38,21 +38,28 @@ const ProfileLayout = () => {
         <Sidebar />
       </div>
 
-      <div className='container flex mx-auto flex-row gap-4 p-4 w-full'>
-        {!isSidebarOpen && (
-          <div className='md:hidden z-50'>
-            <Button type='text' onClick={() => setIsSidebarOpen((prev) => !prev)} className='!p-0'>
-              <MenuOutlined className='text-2xl' />
-            </Button>
+      <div className='flex mx-auto flex-row gap-4 p-4 w-full justify-center items-center'>
+        <div className='container'>
+          {!isSidebarOpen && (
+            <div className='md:hidden z-50'>
+              <Button
+                type='text'
+                onClick={() => setIsSidebarOpen((prev) => !prev)}
+                className='!p-0'
+              >
+                <MenuOutlined className='text-2xl' />
+              </Button>
+            </div>
+          )}
+          <div className='flex justify-center gap-2 flex-row'>
+            <div className='hidden md:block top-0 left-0 min-h-[80vh] z-40 w-1/5'>
+              <Sidebar />
+            </div>
+
+            <div className='md:col-span-3 lg:col-span-3 xl:col-span-4 2xl:col-span-4 w-3/4'>
+              <Outlet />
+            </div>
           </div>
-        )}
-
-        <div className='hidden md:block top-0 left-0 min-h-[80vh] z-40 w-1/5'>
-          <Sidebar />
-        </div>
-
-        <div className='md:col-span-4 lg:col-span-3 xl:col-span-4 2xl:col-span-4 h-full w-3/4'>
-          <Outlet />
         </div>
       </div>
     </div>
