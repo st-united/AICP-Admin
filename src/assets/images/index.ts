c80b83cf9c02a10936720e096b9b82c8d9c6aab6
thongtoan-stunited <thongtoan.nguyen@stunited.vn>
2025-05-20 13:36:36 +0700

    feat: [AICP-10] change UI/Admin Sign-in page and update css global variable

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,5 +1,6 @@
 import background from './background.png';
+import loginBackground from './login-background.png';
 import logoDevPlus from './logo-devplus.svg';
 import logo from './logo.svg';
 
-export { logo, logoDevPlus, background };
+export { logo, logoDevPlus, background, loginBackground };
