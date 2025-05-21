d5f23a25a3406f61492aaf680cd20522f86736c2
VoHoangKiet <muoivo.051064@gmail.com>
2025-05-21 14:22:26 +0700

    refactor: [AICP-16] remove import err

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,5 +1,4 @@
 export * from './Breadcrumb/Breadcrumb';
-export * from './DropProfile/DropProfile';
 export * from './Modal/OpenModal';
 export * from './Modal/Modal';
 export * from './EmptyData/EmptyData';
