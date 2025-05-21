e2cc72844140858cf047785771513a472b27d5c2
thongtoan-stunited <thongtoan.nguyen@stunited.vn>
2025-05-21 10:14:45 +0700

    feat: [AICP-10] refactor tailwind and add notification when login success

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -12,7 +12,7 @@ import { Credentials } from '@app/interface/user.interface';
 import './SignIn.scss';
 
 const SignIn = () => {
-  const { mutate: loginUser } = useLogin();
+  const { mutate: loginUser, isLoading } = useLogin();
   const { t } = useTranslation();
   const navigate = useNavigate();
   const [form] = Form.useForm();
@@ -28,12 +28,12 @@ const SignIn = () => {
     <div id='container-sign-in' className='flex justify-center items-center w-[20rem] md:w-[40%]'>
       <div className='w-full h-full'>
         <div>
-          <h1 className='signin-title text-[1.5rem] text-center md:text-[2rem] lg:text-[2.5rem] font-bold mb-8'>
+          <h1 className='text-primary text-[1.5rem] text-center md:text-[2rem] lg:text-[2.5rem] font-bold mb-8'>
             {t('LOGIN.TEXT')}
           </h1>
         </div>
         <Form form={form} layout='vertical' onFinish={onFinish} className='grid grid-cols-2 gap-4'>
-          <Form.Item className='col-span-2 ' name='email' rules={validator}>
+          <Form.Item className='col-span-2' name='email' rules={validator}>
             <Input
               className='!px-6 !py-4 !rounded-md !text-lg'
               placeholder={t<string>('LOGIN.EMAIL')}
@@ -41,7 +41,7 @@ const SignIn = () => {
           </Form.Item>
           <Form.Item className='col-span-2 ' name='password' rules={validator}>
             <Input.Password
-              className='col-span-2 text-lg !px-6 !py-4 !rounded-md '
+              className='col-span-2 text-lg !px-6 !py-4 !rounded-md'
               placeholder={t<string>('LOGIN.PASSWORD')}
               iconRender={(visible) =>
                 visible ? (
@@ -57,7 +57,10 @@ const SignIn = () => {
               className='text-lg cursor-pointer transition-color duration-3000 border-none !outline-none !bg-transparent'
               onClick={() => navigate('/forgot-password')}
             >
-              <Link to='/forgot-password' className='signin-link font-bold underline'>
+              <Link
+                to='/forgot-password'
+                className='!text-primary-bold font-bold underline hover:!text-primary-light'
+              >
                 {t('LOGIN.FORGOT_PASSWORD')}
               </Link>
             </Button>
@@ -66,7 +69,8 @@ const SignIn = () => {
             <Button
               type='primary'
               htmlType='submit'
-              className='w-full h-[3.5rem] signin-btn text-[1rem] font-bold border-none !outline-none !rounded-md cursor-pointer !transition-colors duration-3000 hover:!text-black'
+              className='w-full h-[3.5rem] !bg-primary-bold text-[1rem] font-bold border-none !outline-none !rounded-md cursor-pointer !transition-colors duration-3000 hover:!text-black'
+              loading={isLoading}
             >
               {t('LOGIN.LOGIN')}
             </Button>
