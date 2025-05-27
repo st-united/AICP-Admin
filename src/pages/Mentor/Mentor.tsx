75229181944b0587083c6076276f98e82105d01f
VoHoangKiet <muoivo.051064@gmail.com>
2025-05-27 17:54:00 +0700

    feat: [AICP-56] implement create mentor

--- src/pages/Mentor/Mentor.tsx
+++ src/pages/Mentor/Mentor.tsx
@@ -1,10 +1,33 @@
-import { Button, Typography } from 'antd';
+import { Button, Form, Typography } from 'antd';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { MentorTable, Summary } from './MentorList';
+import MentorUpSertModal from '@app/components/molecules/Modal/MentorUpSertModal';
+import { useCreateMentor } from '@app/hooks';
 
 const Mentor = () => {
   const { t } = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [form] = Form.useForm();
+
+  const { mutate: createMentor } = useCreateMentor();
+
+  const showMentorUpSertModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleMentorCancel = () => {
+    setIsModalOpen(false);
+    form.resetFields();
+  };
+
+  const handleMentorCreate = async () => {
+    const values = await form.validateFields();
+    createMentor(values);
+    setIsModalOpen(false);
+    form.resetFields();
+  };
   return (
     <div className='flex flex-col mt-2 gap-6 px-5 overflow-y-auto pb-6'>
       <div className='flex justify-between items-center'>
@@ -15,13 +38,23 @@ const Mentor = () => {
           <Button className='flex items-center justify-center rounded-lg shadow-custom border-none text-lg px-6 py-5 text-[#FE7743] font-bold'>
             {t('MENTOR.EXPORT')}
           </Button>
-          <Button className='flex items-center justify-center rounded-lg  shadow-custom border-none text-lg px-6 py-5 text-white font-bold bg-[#FE7743]'>
+          <Button
+            className='flex items-center justify-center rounded-lg shadow-custom border-none text-lg px-6 py-5 text-white font-bold bg-[#FE7743] hover:text-orange-500 active:bg-orange-500 focus:text-orange-500 focus:shadow-lg focus:border-orange-500 focus:border-2'
+            onClick={showMentorUpSertModal}
+          >
             {t('MENTOR.ADD')}
           </Button>
         </div>
       </div>
       <Summary total={0} active={0} inactive={0} />
       <MentorTable />
+      <MentorUpSertModal
+        isOpen={isModalOpen}
+        onCancel={handleMentorCancel}
+        onOk={handleMentorCreate}
+        form={form}
+        isUpdate={false}
+      />
     </div>
   );
 };
